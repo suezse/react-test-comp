c851d473e08d2ef34891659ee6d47f3316d57134
Zhang <sue.s.zhang@kpmg.com>
2018-07-19 17:08:09 +0800

    ''

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Button = () => {
+    return (
+      <div style={{width:'120px', height:'50px', backgroundColor:'red'}}>
+      自制按钮
+      </div>
+    )
+  }
+export default Button;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Button from './components/Button';
+
+module.exports = {
+  Button,
+};
